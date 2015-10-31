54675a166890767ff209a0e07852d751db81ce27
Tufnell <mattlaver@gmail.com>
2015-10-31 22:09:34 +0000

    tidy up js

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ var academy = require('./timetables/academy.json');
 var hanger = require('./timetables/hanger.json');
 var station = require('./timetables/station.json');
 var timetable = require('./timetable.js');
-var port = process.env.PORT || 4200
+var port = process.env.PORT || 4200;
 
 app.use(express.static('public'));
 
-app.get('*', function(req, res,next) {
+app.get('*', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(client) {
+io.on('connection', function (client) {
 
     function broadcastTimes() {
         var dateNow = new Date();
@@ -26,14 +26,14 @@ io.on('connection', function(client) {
             academy: timetable.leavingSoon(dateNow, academy, 'Academy Departures'),
             hanger: timetable.leavingSoon(dateNow, hanger, 'Hanger 89 Departures'),
             station: timetable.leavingSoon(dateNow, station, 'Station Departures')
-        }
+        };
 
         client.broadcast.emit('latestTimes', times);
     }
 
     broadcastTimes();
 
-    setInterval(function() {
+    setInterval(function () {
         broadcastTimes();
     }, 2000);
 
--- public/js/ejbus.js
+++ public/js/ejbus.js
@@ -1,19 +1,18 @@
-var board = new DepartureBoard (document.getElementById ('academy'), { rowCount: 8, letterCount: 26 });
+var board = new DepartureBoard(document.getElementById('academy'), {rowCount: 8, letterCount: 26});
 
-board.setValue (['Academy Bus Timetable']);
+board.setValue(['Academy Bus Timetable']);
 
-var host = location.origin.replace(/^http/, 'ws')
+var host = location.origin.replace(/^http/, 'ws');
 var socket = io.connect(host);
 
 var url = document.URL;
 var isStation = url.toLowerCase().indexOf("station") > -1;
 var isHanger = url.toLowerCase().indexOf("hanger") > -1;
 
-socket.on('latestTimes', function(data) {
+socket.on('latestTimes', function (data) {
     if (isStation) {
-        board.setValue (data.station);
+        board.setValue(data.station);
     } else if (isHanger) {
-        board.setValue (data.hanger);
-    } else board.setValue (data.academy);
-
-})
+        board.setValue(data.hanger);
+    } else board.setValue(data.academy);
+});
